72e08776115530b57ee510260fc40b218b650226
JHS <dkfwl24@gmail.com>
2022-03-07 21:02:58 +0900

    중간 commit

---
+++ src/main/java/com/jhs/book/springboot/config/WebConfig.java
@@ -0,0 +1,4 @@
+package com.jhs.book.springboot.config;
+
+public class WebConfig {
+}
---
+++ src/test/java/com/jhs/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,25 @@
+package com.jhs.book.springboot.web.dto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void 롬복_기능_테스트() {
+        String name = "test";
+        int amount = 1000;
+
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+
+}
